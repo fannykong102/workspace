4c58efa7b77571fecf82cf52a6b38b361212951f
fannykong102 <109B31869@mailst.cjcu.edu.tw>
2021-04-08 16:07:00 +0800

    AdapterHave'nFinish

---
+++ app/src/main/java/com/example/project/personData.java
@@ -0,0 +1,27 @@
+package com.example.project;
+
+public class personData {
+    double height;
+    double weight;
+
+    public personData(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
